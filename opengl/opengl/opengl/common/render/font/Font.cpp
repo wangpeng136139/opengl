577033e6cfee67d3b012172a18d93fe4996819e8
wangpeng <383765662@qq.com>
2024-05-08 21:07:33 +0800

    【优化font】

--- opengl/opengl/opengl/common/render/font/Font.cpp
+++ opengl/opengl/opengl/common/render/font/Font.cpp
@@ -11,6 +11,9 @@ using std::ios;
 std::unordered_map<std::string, Font*> Font::font_map_;
 
 void Font::LoadCharacter(char ch) {
+    if (character_map_[ch] != nullptr) {
+        return;
+    }
     //加载这个字的字形,加载到 m_FTFace上面去;Glyph：字形，图形字符 [glif];
     FT_Load_Glyph(ft_face_, FT_Get_Char_Index(ft_face_, ch), FT_LOAD_DEFAULT);
 
@@ -20,10 +23,41 @@ void Font::LoadCharacter(char ch) {
     //渲染为256级灰度图
     FT_Glyph_To_Bitmap(&ft_glyph, ft_render_mode_normal, 0, 1);
 
+
+
     FT_BitmapGlyph ft_bitmap_glyph = (FT_BitmapGlyph)ft_glyph;
     FT_Bitmap& ft_bitmap = ft_bitmap_glyph->bitmap;
     font_texture_->UpdateSubImage(0, 0, ft_bitmap.width, ft_bitmap.rows, GL_RED, GL_UNSIGNED_BYTE, ft_bitmap.buffer);
+    //存储字符信息
+        //计算新生成的字符，在图集中的排列。
+    if (font_texture_fill_x + ft_bitmap.width >= font_texture_size_) {//从左上角往右上角填充，满了就换一行。
+        font_texture_fill_x = 0;
+        font_texture_fill_y += font_size_;
+    }
+    Character* character = new Character(font_texture_fill_x * 1.0f / font_texture_size_, font_texture_fill_y * 1.0f / font_texture_size_, (font_texture_fill_x + ft_bitmap.width) * 1.0f / font_texture_size_, (font_texture_fill_y + ft_bitmap.rows) * 1.0f / font_texture_size_);
+    character_map_[ch] = character;
+
+    font_texture_fill_x += ft_bitmap.width;
 }
+
+std::vector<Font::Character*> Font::LoadStr(std::string str) {
+    //生成所有的字符 bitmap
+    for (auto ch : str) {
+        LoadCharacter(ch);
+    }
+    //返回所有字符信息
+    std::vector<Character*> character_vec;
+    for (auto ch : str) {
+        auto character = character_map_[ch];
+        if (character == nullptr) {
+            spdlog::error("LoadStr error,no bitmap,ch:{}", ch);
+            continue;
+        }
+        character_vec.push_back(character);
+    }
+    return character_vec;
+}
+
 
 
 Font* Font::GetFont(std::string font_file_path) {
--- opengl/opengl/opengl/common/render/font/Font.h
+++ opengl/opengl/opengl/common/render/font/Font.h
@@ -6,6 +6,20 @@
 
 class Font
 {
+    /// 记录单个字符在图集上的坐标、宽高，用于生成同尺寸的顶点数据，1：1渲染。
+    struct Character {
+        float left_top_x_;
+        float left_top_y_;
+        float right_bottom_x_;
+        float right_bottom_y_;
+        Character(float left_top_x, float left_top_y, float right_bottom_x, float right_bottom_y) {
+            left_top_x_ = left_top_x;
+            left_top_y_ = left_top_y;
+            right_bottom_x_ = right_bottom_x;
+            right_bottom_y_ = right_bottom_y;
+        }
+    };
+
 public:
     /// freetype为字符生成bitmap
     /// \param c
@@ -20,7 +34,10 @@ private:
     FT_Face ft_face_;
     Texture2D* font_texture_;
     unsigned short font_texture_size_ = 1024;
-
+    std::unordered_map<char, Character*> character_map_;//已经生成bitmap的字符
+    std::vector<Character*> LoadStr(std::string str);
+    unsigned short font_texture_fill_x = 0;//当前行的起始点
+    unsigned short font_texture_fill_y = 0;//新的一行的纵向起始点
 public:
 	static Font* LoadFromFile(std::string font_file_path, unsigned short font_size);
 	static Font* GetFont(std::string font_file_path);
