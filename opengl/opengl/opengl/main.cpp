9934818fde4066bb0038ed46394d5553fa97d5eb
wangpeng136139 <383765662@qq.com>
2024-06-20 09:33:09 +0800

    【spscqueue】

--- opengl/opengl/opengl/main.cpp
+++ opengl/opengl/opengl/main.cpp
@@ -3,22 +3,22 @@
 #include "common/common/Application.h"
 
 
-//extern int draw_base();
-//extern int draw_triangle();
-//extern int draw_quad();
-//extern int draw_cube();
-//extern int draw_load_image();
-//extern int draw_cube_vertex();
-//extern int draw_cube_vertex3_3();
-//extern int draw_load_mesh_file();
-//extern int draw_use_shader_file();
-//extern int draw_use_material();
-//extern int draw_mesh_render();
-//extern int draw_use_mesh_render_gameobject();
-//extern int draw_use_more_camera();
-//extern int draw_use_camera();
-//extern int draw_use_key_mouse_control();
-//extern int draw_use_font();
+extern int draw_base();
+extern int draw_triangle();
+extern int draw_quad();
+extern int draw_cube();
+extern int draw_load_image();
+extern int draw_cube_vertex();
+extern int draw_cube_vertex3_3();
+extern int draw_load_mesh_file();
+extern int draw_use_shader_file();
+extern int draw_use_material();
+extern int draw_mesh_render();
+extern int draw_use_mesh_render_gameobject();
+extern int draw_use_more_camera();
+extern int draw_use_camera();
+extern int draw_use_key_mouse_control();
+extern int draw_use_font();
 int main(void)
 { 
 	Application::Init();
